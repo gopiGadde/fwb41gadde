67816b67b87a56bacedccb91609bc2e2bb447f58
gopiGadde <gopi05191998@gmail.com>
2021-11-02 22:42:43 -0500

    added

--- routes/Toyota.js
+++ routes/Toyota.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Toyota', { title: 'Search Results by Toyota' });
+  res.render('toyota', { title: 'Search Results by Toyota' });
 });
 
 module.exports = router;
